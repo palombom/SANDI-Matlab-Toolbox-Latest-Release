5ad3274b64258b8ace23a6be87b43b2e0cc1d103
Marco Palombo <33546086+palombom@users.noreply.github.com>
2023-03-24 10:52:20 +0000

    Update train_RF_matlab.m

--- functions/ML_fitting/train_RF_matlab.m
+++ functions/ML_fitting/train_RF_matlab.m
@@ -1,4 +1,4 @@
-function Mdl = train_RF_matlab(database_train, params_train, n_trees)
+function trainedML = train_RF_matlab(database_train, params_train, n_trees)
 % Train a Random Forest regressor for SANDI fitting
 %
 % Author:
@@ -10,15 +10,27 @@ function Mdl = train_RF_matlab(database_train, params_train, n_trees)
 
 tic
 
+trainedML = struct;
 Mdl = cell(size(params_train,2),1);
+MLprediction = zeros(size(params_train));
+Rsq = zeros(size(params_train,2),1);
+Slope = zeros(size(params_train,2),1);
+Intercept = zeros(size(params_train,2),1);
 
 rng(1);
 
 parfor i=1:size(params_train,2)
     
     Mdl{i} = TreeBagger(n_trees,database_train,params_train(:,i),'Method','regression','OOBPrediction','On');
+    MLprediction(:,i) = predict(Mdl{i},database_train);
+    [Rsq(i),Slope(i),Intercept(i)] = regression(params_train(:,i)', MLprediction(:,i)');
 
 end
+
+trainedML.Mdl = Mdl;
+trainedML.Rsq = Rsq;
+trainedML.Slope = Slope;
+trainedML.Intercept = Intercept;
 
 tt = toc;
 
