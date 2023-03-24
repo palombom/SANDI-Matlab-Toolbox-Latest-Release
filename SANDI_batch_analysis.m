799339f0ddb1320412e5608d599c610bc3763847
Marco Palombo <33546086+palombom@users.noreply.github.com>
2023-03-24 10:51:36 +0000

    Update SANDI_batch_analysis.m

--- SANDI_batch_analysis.m
+++ SANDI_batch_analysis.m
@@ -78,6 +78,9 @@ SANDIinput.smalldel = ; % <---- USER DEFINED: the diffusion gradient duration ( 
 SANDIinput.Nset = 1e4; % Size of the training set. Recommended values: 1e4 for testing the performance and between 1e5 and 1e6 for the analysis. Do not use values < 1e4. Values >1e6 might lead to 'out of memory'. 
 SANDIinput.MLmodel = 'MLP'; % can be 'RF' for Random Forest (default); 'MLP' for multi-layer perceptron
 SANDIinput.FittingMethod = 0; % can be '0': minimizes MSE between ground truth model parameters and ML predictions. It has higher precision but lower accuracy; or '1': minimizes MSE between NLLS estimates of model parameters and ML predictions. It has higher accuracy but lower precision.
+SANDIinput.MLdebias = 1; % can be '0' or '1'. When '1', it will estimate slope and intercept from the prediciton vs ground truth relation (from training set) and correct the prediciton to follow the unit line. 
+
+SANDIinput.FWHM = 0.001; % size of the 3D Gaussian smoothing kernel. If needed, this smooths the input data before analysis.
 
 SANDIinput.DoTestPerformances = 1; % If '1' then it compares the performances of ML estimation with NLLS estimation on unseen simulated signals and write HTML report
 SANDIinput.diagnostics = 1; % can be '0' or '1'. When '1', figures will be plotted to help checking the results
