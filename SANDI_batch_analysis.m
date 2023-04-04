45b0a2909257acbfcff641d7fe08a2e3bdea8bcd
Marco Palombo <33546086+palombom@users.noreply.github.com>
2023-04-04 21:31:59 +0000

    Update SANDI_batch_analysis.m

--- SANDI_batch_analysis.m
+++ SANDI_batch_analysis.m
@@ -76,7 +76,7 @@ SANDIinput.smalldel = ; % <---- USER DEFINED: the diffusion gradient duration ( 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% END %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 SANDIinput.Nset = 1e4; % Size of the training set. Recommended values: 1e4 for testing the performance and between 1e5 and 1e6 for the analysis. Do not use values < 1e4. Values >1e6 might lead to 'out of memory'. 
-SANDIinput.MLmodel = 'MLP'; % can be 'RF' for Random Forest (default); 'MLP' for multi-layer perceptron
+SANDIinput.MLmodel = 'MLP'; % can be 'RF' for Random Forest; 'MLP' for multi-layer perceptron
 SANDIinput.FittingMethod = 0; % can be '0': minimizes MSE between ground truth model parameters and ML predictions. It has higher precision but lower accuracy; or '1': minimizes MSE between NLLS estimates of model parameters and ML predictions. It has higher accuracy but lower precision.
 SANDIinput.MLdebias = 1; % can be '0' or '1'. When '1', it will estimate slope and intercept from the prediciton vs ground truth relation (from training set) and correct the prediciton to follow the unit line. 
 
